e2d127d5828a306c4a3eeb5ed229967ef5e5e5ea
Sandesh Bhandari <sandesh_bhandari007@hotmail.com>
2019-03-04 21:53:14 -0500

    Updated daemon file - changes needed for import grpc files to work

--- daemon.py
+++ daemon.py
@@ -1,13 +1,20 @@
 import grpc
+import time
+import logging
+from concurrent import futures
 from worker import Worker
-TaskService = __import__("etherfx-core.ether.net.proto.TaskService_pb2_grpc.TaskService")
 
+from TaskService_pb2_grpc import TaskServiceServicer as TaskService
+from TaskService_pb2_grpc import add_TaskServiceServicer_to_server as AddTaskServicer
+
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
 class Daemon(TaskService):
 
     def __init__(self):
-        self.args = None
-        self.task_id = None
-        self.task_metadata = None
+        # self.args = None
+        # self.task_id = None
+        # self.task_metadata = None
         pass
 
     def generateTaskID(self, TaskMetaData):
@@ -16,23 +23,32 @@ class Daemon(TaskService):
     def spinUpNewNode(self):
         pass
 
-    def add_task(self, request, response):
-        pass
+    def AddTask(self, request, context):
+        print("This is working!!")
 
-    def add_argument(self, request, response):
+    def AddArgument(self, request, context):
         # p = ThreadPool(self.task_metadata.noofargs)
         # self.args.append(p.map(Worker, request.<stream-name>))
         pass
 
-    def poll_task(self, request, response):
+    def PollTask(self, request, context):
         pass
 
-    def exec_task(self, request, response):
+    def ExecTask(self, request, context):
         pass
 
-    def serve(self):
-        pass
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    AddTaskServicer(Daemon(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
 
 if __name__ == "__main__":
     	worker = Daemon()
-    	worker.serve()
+    	serve()
